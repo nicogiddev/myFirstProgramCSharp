d4bed44fc42f2e1b07f11b4c490ce7a35b18f3be
gidon <gidon@LAPTOP-MGFN4033>
2017-01-24 10:06:15 +0100

    Add Read()

--- myFirstProgramCSharp/myFirstProgramCSharp/Program.cs
+++ myFirstProgramCSharp/myFirstProgramCSharp/Program.cs
@@ -11,6 +11,7 @@ namespace myFirstProgramCSharp
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World !");
+            Console.Read();
         }
     }
 }
