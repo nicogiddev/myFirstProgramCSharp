069fbf5f25e5a9db2fc8e87b2d6226025128394a
gidon <gidon@LAPTOP-MGFN4033>
2017-01-25 12:40:17 +0100

    add if/else/for sample

--- myFirstProgramCSharp/myFirstProgramCSharp/Program.cs
+++ myFirstProgramCSharp/myFirstProgramCSharp/Program.cs
@@ -12,9 +12,43 @@ namespace myFirstProgramCSharp
         {
             Console.ForegroundColor = ConsoleColor.Blue;
             Console.BackgroundColor = ConsoleColor.Yellow;
-            string name = Console.ReadLine();
-            Console.WriteLine("Hello World !" + name);
-            Console.Read();
+
+            // Jouer avec des For
+
+            for (int x = 0; x <= 10; x++)
+            {
+                Console.WriteLine("La valeur de x : " + x.ToString());
+
+                if (x == 7)
+                {
+                    Console.WriteLine("Trouvé la valeur 7");
+                    break;
+                }
+            }
+
+            // Jouer avec des If/Else
+
+            Console.WriteLine("Saisissez le type d'opération Multiplier (m) ou Diviser (d) : ");
+            String decision = Console.ReadLine();
+            Console.WriteLine("Saisisez un chiffre");
+            String chaineChiffre = Console.ReadLine();
+            int chiffre = Convert.ToInt32(chaineChiffre);
+
+            if (decision == "m")
+            {
+                chiffre *= chiffre;
+                Console.WriteLine(" Résultat de la multilication = " + chiffre.ToString());
+            }
+            else if (decision == "d")
+            {
+                chiffre /= chiffre;
+                Console.WriteLine(" Résultat  de la division = " + chiffre.ToString());
+            }
+            else
+            {
+                Console.WriteLine("Vous n'avez pas saissi une information correcte.");
+            }
+            Console.Read(); 
         }
     }
 }
